e2f4c3c85db705431bf7bfa9845e66523b900f68
Krisztian Domokos <domokoskrisztian7@gmail.com>
2024-12-18 16:16:02 +0200

    SDL2 setup

--- pong/main.go
+++ pong/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-func main() {
+const winWidth, winHeight = 800, 600
 
-	winWidth := 800
-	winHeight := 600
+type color struct {
+	r, g, b byte
+}
+
+func setPixel(x, y int, c color, pixels []byte) {
+	index := (y*winWidth + x) * 4
+
+	if index < len(pixels)-4 && index >= 0 {
+		pixels[index] = c.r
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
+	}
+}
+
+func main() {
 
 	window, err := sdl.CreateWindow("Go SDL2 Window", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
 	if err != nil {
@@ -25,7 +39,7 @@ func main() {
 	}
 	defer renderer.Destroy()
 
-	tex, err := sdl.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(winWidth), int32(winHeight))
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(winWidth), int32(winHeight))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -34,6 +48,16 @@ func main() {
 	defer tex.Destroy()
 
 	pixels := make([]byte, winWidth*winHeight*4)
+
+	for y := 0; y < winHeight; y++ {
+		for x := 0; x < winWidth; x++ {
+			setPixel(x, y, color{255, 0, 0}, pixels)
+		}
+	}
+
+	tex.Update(nil, unsafe.Pointer(&pixels[0]), winWidth*4)
+	renderer.Copy(tex, nil, nil)
+	renderer.Present()
 
 	sdl.Delay(2000)
 
